36a46aba485233196d17fe5b7ba5dc50f1a48bbb
PunkDumpling <143641620+PunkDumpling@users.noreply.github.com>
2024-12-03 03:08:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.querySelectorAll('.grid-item').forEach(item => {
-    item.addEventListener('click', () => {
-        document.body.style.cursor = 'url(path-to-your-cursor-icon.cur), auto';
-    });
-});
 
-    // Чтобы вернуть курсор обратно при клике на другую часть страницы:
-document.body.addEventListener('click', (event) => {
-    if (!event.target.classList.contains('grid-item')) {
-        document.body.style.cursor = 'default';
-    }
-});
